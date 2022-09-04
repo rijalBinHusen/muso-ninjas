568e6eeab6841200abca5994dd463fe5b3866ee6
rijal <jeryzal333@gmail.com>
2022-09-04 11:04:21 +0700

    navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
- <div class="content">
-  <router-view/>
- </div>
+<div>
+  <Navbar />
+  <div class="content">
+    <router-view/>
+  </div>
+</div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  setup() {
+    
+  },
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
   .content {
