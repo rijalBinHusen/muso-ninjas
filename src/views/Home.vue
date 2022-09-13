c684146b057203215b708c8c7c8651f6c5bf2062
rijal <jeryzal333@gmail.com>
2022-09-14 02:49:11 +0700

    get realtime playlist data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="home">
-    <h1>Hello world</h1>
+    <div class="error" v-if="error"> Could not fetch the data </div>
+    <div v-if="documents">
+      <div v-for="doc in documents" :key="doc.id">
+        {{ doc.title }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import getCollection from '../composable/getCollection'
 
 export default {
   name: 'Home',
+  setup() {
+    const { error, documents } = getCollection('playlists')
+
+    return { error, documents }
+  },
   components: {
   }
 }
