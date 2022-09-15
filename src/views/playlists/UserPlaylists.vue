65b49c26d94a168a250bee939269554701ac1fc4
rijalBinHusen <jeryzal333@gmail.com>
2022-09-15 08:14:18 +0700

    querying user playlists

--- src/views/playlists/UserPlaylists.vue
+++ src/views/playlists/UserPlaylists.vue
@@ -3,3 +3,23 @@
         <h2>My playlist</h2>
     </div>
 </template>
+
+<script>
+    import getCollection from '../../composable/getCollection';
+    import getUser from '../../composable/getUser';
+
+    export default {
+        setup () {
+            const { user } = getUser()
+
+            const { documents: playlists } = getCollection(
+                                                            'playlists', 
+                                                            ['userId', '==', user.value.uid]
+                                                        )
+            
+            console.log(playlists)
+            return { playlists }
+        }
+    }
+
+</script>
